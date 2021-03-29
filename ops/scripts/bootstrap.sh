1689a10cc4b360e49cd6431199541069af29fba4
evilsysadmin <trainingit@zoho.com>
2021-03-29 16:02:29 +0200

    yaml for extra cluster features

--- ops/scripts/bootstrap.sh
+++ ops/scripts/bootstrap.sh
@@ -16,6 +16,7 @@ CONFIGMAP_RELOAD_VERSION=configmap-reload:v0.5.0
 NODE_EXPORTER_VERSION=v1.0.1
 PROMETHEUS_VERSION=v2.24.0
 HELM_VERSION=v3.5.3
+ELASTIC_VERSION=7.12.0
 
 K9S_BINARY="k9s"
 K3D_BINARY="k3d"
@@ -27,6 +28,22 @@ k3D_FILE="k3d-linux-amd64"
 SKAFFOLD_FILE="skaffold-linux-amd64"
 KUBECTL_ARCH="linux"
 
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 
 # function to download basic binaries
 
@@ -66,6 +83,17 @@ function bootstrap() {
   fi
 }
 
+#################################################################################
+## START OF SCRIPT
+#################################################################################
+
+# parse features
+
+eval $(parse_yaml config.yaml)
+
+echo "Creating cluster with the following extra features: "
+echo "..."
+
 # check architecture - darmin or amd64
 
 if [[ `uname` == 'Linux' ]]; then
@@ -83,18 +111,35 @@ k3d cluster create ${CLUSTER_NAME} --registry-create --api-port 127.0.0.1:6443 -
 -p 443:443@loadbalancer --k3s-server-arg "--no-deploy=traefik"
 
 # pull images locally
+
+echo -n "${LIGHTRED}[!] ${WHITE}Waiting for docker to pull all images "
+
 docker pull rancher/metrics-server:${RANCHER_METRICS_SERVER} > /dev/null 2>&1 &
 docker pull rancher/klipper-lb:${RANCHER_KLIPPER_VERSION} > /dev/null 2>&1 &
-docker pull localstack/localstack:${LOCALSTACK_VERSION} > /dev/null 2>&1 &
 docker pull traefik:${TRAEFIK_VERSION} > /dev/null 2>&1 &
 docker pull rancher/coredns-coredns:${COREDNS_VERSION} > /dev/null 2>&1 &
 docker pull k8s.gcr.io/kube-state-metrics/kube-state-metrics:${KUBESTATE_METRICS_VERSION} > /dev/null 2>&1 &
-docker pull quay.io/prometheus/alertmanager:${PROMETHEUS_ALERT_MANAGER_VERSION} > /dev/null 2>&1 &
-docker pull quay.io/prometheus/node-exporter:${NODE_EXPORTER_VERSION} > /dev/null 2>&1 &
-docker pull quay.io/prometheus/prometheus:${PROMETHEUS_VERSION} > /dev/null 2>&1 &
-docker pull jimmidyson/${CONFIGMAP_RELOAD_VERSION} > /dev/null 2>&1 &
 
-echo -n "${LIGHTRED}[!] ${WHITE}Waiting for docker to pull all images "
+# pull extra images conditionally
+if [ ${k3d_localstack} = true ]
+then
+  docker pull localstack/localstack:${LOCALSTACK_VERSION} > /dev/null 2>&1 &
+fi
+
+
+if [ ${k3d_prometheus} = true ]
+then
+  echo "Pulling prometheus images..."
+  docker pull quay.io/prometheus/alertmanager:${PROMETHEUS_ALERT_MANAGER_VERSION} > /dev/null 2>&1 &
+  docker pull quay.io/prometheus/node-exporter:${NODE_EXPORTER_VERSION} > /dev/null 2>&1 &
+  docker pull quay.io/prometheus/prometheus:${PROMETHEUS_VERSION} > /dev/null 2>&1 &
+  docker pull jimmidyson/${CONFIGMAP_RELOAD_VERSION} > /dev/null 2>&1 &
+fi
+
+if [ ${k3d_elastic} = true ]
+then
+  docker pull docker.elastic.co/elasticsearch/elasticsearch:${ELASTIC_VERSION}
+fi
 
 while ps ax | grep -v grep | grep "docker pull" > /dev/null
 do
@@ -107,34 +152,49 @@ echo
 # load local images into cluster
 k3d image import --cluster ${CLUSTER_NAME} rancher/metrics-server:${RANCHER_METRICS_SERVER} \
     rancher/klipper-lb:${RANCHER_KLIPPER_VERSION} rancher/coredns-coredns:${COREDNS_VERSION} \
-     localstack/localstack:${LOCALSTACK_VERSION} traefik:${TRAEFIK_VERSION} \
-     k8s.gcr.io/kube-state-metrics/kube-state-metrics:${KUBESTATE_METRICS_VERSION} \
-     quay.io/prometheus/node-exporter:${NODE_EXPORTER_VERSION} \
-     quay.io/prometheus/alertmanager:${PROMETHEUS_ALERT_MANAGER_VERSION} \
-     quay.io/prometheus/prometheus:${PROMETHEUS_VERSION} \
-     jimmidyson/${CONFIGMAP_RELOAD_VERSION}
+    k8s.gcr.io/kube-state-metrics/kube-state-metrics:${KUBESTATE_METRICS_VERSION} \
+    traefik:${TRAEFIK_VERSION} > /dev/null 2>&1 &
 
-# create localstack setup
-kubectl apply -f ops/yaml/localstack/deployment.yaml
-kubectl apply -f ops/yaml/localstack/ingress.yaml
-kubectl apply -f ops/yaml/localstack/service.yaml
+if [ ${k3d_localstack} = true ]
+then
+  k3d image import --cluster ${CLUSTER_NAME} localstack/localstack:${LOCALSTACK_VERSION}  > /dev/null 2>&1 &
+  # create localstack setup
+  kubectl apply -f ops/yaml/localstack/deployment.yaml
+  kubectl apply -f ops/yaml/localstack/ingress.yaml
+  kubectl apply -f ops/yaml/localstack/service.yaml
+fi
+
+if [ ${k3d_prometheus} = true ]
+then
+  echo "installing prometheus..."
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  k3d image import --cluster ${CLUSTER_NAME} quay.io/prometheus/node-exporter:${NODE_EXPORTER_VERSION} \
+   quay.io/prometheus/alertmanager:${PROMETHEUS_ALERT_MANAGER_VERSION} \
+   quay.io/prometheus/prometheus:${PROMETHEUS_VERSION} \
+   jimmidyson/${CONFIGMAP_RELOAD_VERSION} > /dev/null 2>&1 &
+   kubectl create ns lens-metrics
+   helm install prometheus prometheus-community/prometheus --namespace lens-metrics
+fi
+
+if [ ${k3d_elastic} = true ]
+then
+  k3d image import --cluster ${CLUSTER_NAME} docker.elastic.co/elasticsearch/elasticsearch:${ELASTIC_VERSION} > /dev/null 2>&1 &
+fi
 
 # setup helm charts
-
 # add repos
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add traefik https://helm.traefik.io/traefik
+helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
+
 helm repo update
 
-kubectl create ns lens-metrics
-
-helm install prometheus prometheus-community/prometheus --namespace lens-metrics
-helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 helm install kube-state-metrics kube-state-metrics/kube-state-metrics -n lens-metrics --namespace lens-metrics
 
 helm install traefik traefik/traefik
 
-# give access to traefik dashboard
-
-# create localstack setup
-kubectl apply -f ops/yaml/traefik/dashboard.yaml
+if [ ${k3d_elastic} = true ]
+then
+  helm repo add elastic https://helm.elastic.co
+  helm install elastic-operator elastic/eck-operator -n elastic-system --create-namespace
+  kubectl apply -f ops/yaml/elastic/cluster.yaml
+fi
--- ops/scripts/waitforpods.sh
+++ ops/scripts/waitforpods.sh
@@ -1,18 +1,50 @@
 #!/usr/bin/env bash
 
-echo -e "waiting for traefik pod"
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 
+eval $(parse_yaml config.yaml)
+
+echo -e "waiting for traefik pod"
 while [[ $(kubectl get pods -l app.kubernetes.io/name=traefik -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done
 kubectl port-forward $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name) 9000:9000 > /dev/null &
 
 echo -e ""
 
-echo -e "waiting for prometheus pods"
+if [ ${k3d_prometheus} = true ]
+then
 
-while [[ $(kubectl get pods -n lens-metrics -l app=prometheus -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True True" ]]; do echo -n "." && sleep 1; done
+  echo -e "waiting for prometheus pods"
+  while [[ $(kubectl get pods -n lens-metrics -l app=prometheus -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True True" ]]; do echo -n "." && sleep 1; done
+  echo -e ""
+fi
 
-echo -e ""
+if [ ${k3d_localstack} = true ]
+then
 
-echo -e "waiting for localstack pod"
+  echo -e "waiting for localstack pod"
+  while [[ $(kubectl get pods -l app=localstack -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done
+  echo -e ""
 
-while [[ $(kubectl get pods -l app=localstack -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done
+fi
+
+if [ ${k3d_elastic} = true ]
+then
+  echo -e "waiting for elastic pod"
+  while [[ $(kubectl get pods -l common.k8s.elastic.co/type=elasticsearch -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done
+  echo -e ""
+fi
